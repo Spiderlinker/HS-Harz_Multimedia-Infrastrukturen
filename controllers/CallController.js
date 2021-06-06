19f292398008398c53e1f6f90a42944a18943589
Robin Kopitz <rkopitz@hs-harz.de>
2021-06-06 18:27:42 +0200

    - webRtcCon.js kommentiert - Call Controller kommentiert

--- controllers/CallController.js
+++ controllers/CallController.js
@@ -1,7 +1,21 @@
 const roomHandler = require('../core/RoomHandler');
 const uuid = require("uuid");
-
+/**
+ * Diese Funktion kontrolliert alle über den übergebenen Socket transportierten Events.
+ * 
+ * @param {socket} socket 
+ */
 const handleSocketConnection = (socket) => {
+
+  /**
+   * Diese Funktion wartet auf einen socket emit mit der Spezifizierung "join". Sollte die dabei übergebene
+   * RaumID null sein handelt es sich um die Erstellung eines neuen Raums. Über das Modul "uuid" wird eine Raum-ID
+   * generiert. Sollte eine RaumID übergeben wurden sein wird diese verwendet.
+   * Über den RoomHandler wird die Anzahl der aktuellen Teilnehmer verarbeitet und es wird
+   * ein Server-Log erzeugt. Sollte die Teilnehmeranzahl 0 entsprechen wird der Raum erzeugt. Bei einer Teilnehmer-
+   * anzahl von 1 wird dem Raum beigetreten. Bei einer Teilnehmeranzahl von 2 ist der Raum voll, da sich
+   * nur 2 Teilnehmer in diesem Raum befinden sollen. Es wird eine Meldung generiert. 
+   */
   socket.on('join', (roomId) => {
 
     if(roomId == null)
@@ -25,7 +39,10 @@ const handleSocketConnection = (socket) => {
   }
   })
 
-  // These events are emitted to all the sockets connected to the same room except the sender.
+  /**
+   * Die nachfolgenden Events werden an alle übermittelt, die mit diesem socket Verbunden sind ausgenommen dem Sender.
+   * Alle Events dienen lediglich der Steuerung / Kommunikation zur Aushandelung der WebRTC Verbindung 
+   */
   socket.on('start_call', (roomId) => {
     console.log(`Broadcasting start_call event to peers in room ${roomId}`)
     socket.broadcast.to(roomId).emit('start_call')
@@ -42,6 +59,12 @@ const handleSocketConnection = (socket) => {
     console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
     socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
   })
+
+  /**
+   * Sollte ein Teilnehmer den Raum verlassen, so wird die aktuelle Nutzeranzahl entsprechend reduziert.
+   * Dieses Event wird automatisch getriggert. Da nur vorgesehen ist, das jeder Nutzer nur einem Raum beitreten kann,
+   * ist es möglich über die List einheitlich den eingetretenen Raum zu bestimmen.
+   */
   socket.on('disconnecting', () => {
     socket.rooms.forEach((value) => {
       if(roomHandler.isPresent(value)){
--- public/assets/js/webRtcCon.js
+++ public/assets/js/webRtcCon.js
@@ -1,6 +1,8 @@
 import messageHandler from './MessageHandler.js';
 
-// DOM elements.
+/**
+ * DOM Elemente zur Steuerung des Views Connection.ejs
+ */
 const roomSelectionContainer = document.getElementById('establish_connection_container')
 const roomInput = document.getElementById('room_id');
 const connectButton = document.getElementById('connect_button');
@@ -38,7 +40,9 @@ let isRoomCreator;
 let rtcPeerConnection; // Connection between the local device and the remote peer.
 let roomId;
 
-// Free public STUN servers provided by Google.
+/**
+ * Standadrd STUN-Server
+ */
 let iceServers = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
@@ -93,6 +97,11 @@ micMuteUnmuteButton.addEventListener('click', () =>{
 });
 
 // SOCKET EVENT CALLBACKS =====================================================
+/**
+ * Diese Methode wird beim Event "room_created" aktiviert. Die RaumID wird gesetzt und die definierten
+ * Media Constraints werden gesetzt. Die boolean Variable isRoomCreator wird auf true gesetz um den Nutzer
+ * als Raumersteller im späteren Prozess wiederzuerkennen. Anschließend wird auf eine Verbindung gewartet.
+ */
 socket.on('room_created', async (room) => {
   console.log('Socket event callback: room_created');
   roomId = room;
@@ -101,17 +110,32 @@ socket.on('room_created', async (room) => {
   isRoomCreator = true;
 });
 
+/**
+ * Diese Methode wird beim Event "room_joined" aktiviert. Die MediaConstraints werden gesetzt und es wird
+ * das Event "start_call" über den Socket übertragen.
+ */
 socket.on('room_joined', async () => {
   console.log('Socket event callback: room_joined');
   await setLocalStream(mediaConstraints);
   socket.emit('start_call', roomId);
 });
 
+/**
+ * Diese Methode wird beim Event "full_room" aktiviert und generiert einen Meldung.
+ */
 socket.on('full_room', () => {
   console.log('Socket event callback: full_room');
-  alert('The room is full, please try another one');
+  alert('Dieser Raum ist aktuell voll!');
 });
 
+/**
+ * Diese Methode initialisiert die rtcPeerConnection, dabei werden die STUN- und TURN-Server 
+ * übergeben. Anschließend werden die Video und Audio Tracks der Verbindung hinzugefügt. Die Eigenschaften
+ * ontrack und onicecandidate werden gesetzt. Hier werden Methoden übergeben die beim eintritt des events
+ * angesprochen werden. In dieser Methode wird noch der Datenchannel für den Chat angelegt. Zum Abschluss
+ * wird ein rtcPeerConnection offer erstellt. Diese Methode wird immer von dem Nutzer ausgeführt, welcher dem erstellten
+ * Raum joined.
+ */
 socket.on('start_call', async () => {
   console.log('Socket event callback: start_call');
 
@@ -124,10 +148,16 @@ socket.on('start_call', async () => {
     messageHandler.setupRoomCreateDataChannel(rtcPeerConnection);
 
     await createOffer(rtcPeerConnection);
-
   }
 });
 
+/**
+ * Diese Methode wird aktiviert sobald die webRTC offer erfolgreich durchlaufen wurde. Sollte es sich um den
+ * Raumersteller handeln, erstellt dieser ebenfalls eine rtcPeerConnection fügt seine Audio und Video tracks hinzu
+ * und setzt die beiden Eigenschaften ontrack und onicecandidate wie in @see socket.on('start_call'). Der Raum ersteller
+ * setzt zusätzlich noch die Remote Description welche die Eigenschaften auf Remote seite zeigen.  Anschließend
+ * tritt er noch dem DataChannel für die Chat Übertragung bei und sented sein webRTC Antwort.
+ */
 socket.on('webrtc_offer', async (event) => {
   console.log('Socket event callback: webrtc_offer');
 
@@ -144,11 +174,19 @@ socket.on('webrtc_offer', async (event) => {
   }
 });
 
+/**
+ * Der Nutzer welcher dem Raum beigetreten ist erhält die WebRTC Antwort und spezifiziert seine Remote Eigenschaften.
+ * Sollte kein WebRTC Ice Event für die Verbindung benötigt werden besteht hier eine Verbindung.
+ */
 socket.on('webrtc_answer', (event) => {
   console.log('Socket event callback: webrtc_answer');
   rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
 });
 
+/**
+ * Diese Methode wird aktiviert sofern die Eigenschaft onicecandidate getriggert wird. Sollte also keine Verbindung aufgebaut werden
+ * können wird über den STUN-Server der NAT-Type bestimmt oder gar über einen TURN-Server der traffic geleitet. 
+ */
 socket.on('webrtc_ice_candidate', (event) => {
   console.log('Socket event callback: webrtc_ice_candidate');
 
@@ -161,8 +199,16 @@ socket.on('webrtc_ice_candidate', (event) => {
 });
 
 // FUNCTIONS ==================================================================
+
+/**
+ * Diese Methode nimmt eine roomID oder null entgegegen, dieser Wert wird über den einen socket.emit
+ * an den Server übermittelt. Der Server erstellt oder tritt dem angegebnen Raum bei. Der videoChatContainer
+ * wird auf sichtbar gestellt alle anderen Container werden ausgeblendet.
+ *   
+ * @param {string} room 
+ * 
+ */
 function joinRoom(room) {
-  console.log(iceServers);
   if (room === '') {
     alert('Bitte geben Sie eine Raum-ID an!');
   } else {
@@ -173,6 +219,18 @@ function joinRoom(room) {
   }
 }
 
+/**
+ * Diese Methode ermöglicht es die bestehenden Container bei bedarf ein- und
+ * auszublenden hierfür muss entweder der String "none" für ausblenden und
+ * "block" für einblenden übergeben werden.
+ * 
+ * @param {string} video - videoChatContainer "none" oder "block"
+ * @param {string} choice - choiceContainer "none" oder "block"
+ * @param {string} room - roomSelectionContainer "none" oder "block"
+ * @param {string} creation - creationContainer "none" oder "block"
+ * @param {string} settings - settingsContainer "none" oder "block"
+ * 
+ */
 function showContainer(video, choice, room, creation, settings){
   videoChatContainer.style = `display: ${video}`;
   choiceContainer.style = `display: ${choice}`;
@@ -181,6 +239,10 @@ function showContainer(video, choice, room, creation, settings){
   settingsContainer.style = `display: ${settings}`;
 }
 
+/**
+ * Diese Methode ermöglicht es einem Nutzer die bestehenden STUN und TURN Server 
+ * zu überschreiben und seine eigenen zu nutzen.
+ */
 function rewriteSettings(){
   iceServers = {
     iceServers: [
@@ -192,6 +254,18 @@ function rewriteSettings(){
   showContainer('none', 'block', 'none', 'none', 'none');
 }
 
+/**
+ * Diese Methode nimmt die oben definierten MediaConstraints entgegen und 
+ * startet eine Berechtigungsanfrage über den Browser des Nutzers. Stimmt
+ * der User der Nutzung von Kamera und Mikrofon zu kann eine webRTC Verbindung
+ * aufgebaut werden. Die Variable localStream wird gesetzt und es wird auf die
+ * gegenstelle gewartet. 
+ * 
+ * @error Sollte ein Fehler bei der Abfrage auftreten oder der Nutzer den Zugriff verweigern
+ * wird eine Fehlermeldung als alert und in der console wiedergegeben. 
+ * 
+ * @param {list} mediaConstraints 
+ */
 async function setLocalStream(mediaConstraints) {
   let stream;
   try {
@@ -205,12 +279,31 @@ async function setLocalStream(mediaConstraints) {
   localVideoComponent.srcObject = stream;
 }
 
+/**
+ * Diese Methode fügt die beiden Tracks Audio und Video der 
+ * rtcPeerConnection hinzu, damit diese übertragen werden können. 
+ * 
+ * @param {interface} rtcPeerConnection 
+ */
 function addLocalTracks(rtcPeerConnection) {
   localStream.getTracks().forEach((track) => {
     rtcPeerConnection.addTrack(track, localStream);
   });
 }
 
+/**
+ * Diese Methode initiert das SDP (Session Description Protocol (peer-to-peer Verbindungen))
+ * um eine WebRTC Verbindung zu starten. Im SDP sind Informationen wie der Kodec, Quelladresse und
+ * die Timing-Informationen für Audio und Video enthalten. Als Ergebnis des offers erhält die Methode
+ * eine sessionDescription welche die Konfiguration angibt, die am lokalen Ende der Verbindung angewendet
+ * werden soll. Diese lokale Beschreibung muss in der rtcPeerConnection gesetzt werden kann bei Bedarf auch
+ * weggelassen werden (Beim weglassen wird versucht die Konfiguration automatisch herzustellen).
+ * 
+ * @error - Sollte eine Verbindungsanfrage fehlschlagen wird der Fehlerfall in der Konsole des Nutzers
+ * ausgegeben.
+ *  
+ * @param {interface} rtcPeerConnection 
+ */
 async function createOffer(rtcPeerConnection) {
   let sessionDescription;
   try {
@@ -227,6 +320,17 @@ async function createOffer(rtcPeerConnection) {
   });
 }
 
+/**
+ * Diese Methode erstellt eine SDP (mehr Informationen zu SDP @see createOffer) antwort auf
+ * eine remote anfrage. Die Antwort enthält alle bereits hinzugefügten Media Track (Audio und Video)
+ * und alle bereits angegeben STUN und TURN (ICE) Server. Diese Antwort wird an den Anfragenden zurück
+ * übermittelt um den Aushandelungsprozess fortzusetzen.
+ * 
+ * @error - Sollte die Erstellung der Antwort fehlschlagen wird der Fehlerfall in der Konsole des Nutzers
+ * ausgegeben.
+ * 
+ * @param {interface} rtcPeerConnection 
+ */
 async function createAnswer(rtcPeerConnection) {
   let sessionDescription;
   try {
@@ -243,11 +347,22 @@ async function createAnswer(rtcPeerConnection) {
   });
 }
 
+/**
+ * Diese Methode setzt den Mediastream des Verbindungspartners auf das dafür vorgesehene
+ * DOM Object.
+ * @param {*} event 
+ */
 function setRemoteStream(event) {
   remoteVideoComponent.srcObject = event.streams[0];
   remoteStream = event.stream;
 }
 
+/**
+ * Diese Methode sendet die vom Nutzer oder bereits vordefinierten STUN- und TURN Server
+ * an den Server über einen Socket emit.
+ * 
+ * @param {*} event 
+ */
 function sendIceCandidate(event) {
   if (event.candidate) {
     socket.emit('webrtc_ice_candidate', {
@@ -258,11 +373,18 @@ function sendIceCandidate(event) {
   }
 }
 
+/**
+ * Diese Methode erlaubt es das Mikrofon zu muten und zu entmuten
+ */
 function muteUnmuteMic(){
   localStream.getAudioTracks()[0].enabled = !(localStream.getAudioTracks()[0].enabled);
   console.log(localStream.getAudioTracks()[0]);
 }
 
+/**
+ * Diese Methode ermöglicht die Kontroller der eigenen Kamera ressource man kann sie an und
+ * abschalten.
+ */
 function disableEnableCam(){
   localStream.getVideoTracks()[0].enabled = !(localStream.getVideoTracks()[0].enabled);
   console.log(localStream.getVideoTracks()[0]);
